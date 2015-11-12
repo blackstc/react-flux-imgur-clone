b39f2f904354c66dc8a1fcdc87fcf1617fae70d6
Blackstock <charlieblackstock@gmail.com>
2015-11-11 22:26:00 -0700

    added routes for topics

---
+++ src/utils/api.jsx
@@ -0,0 +1,15 @@
+var Fetch = require('whatwg-fetch');
+var rootUrl = 'https://api.imgur.com/3/';
+var apiKey = '0c81f59f19061f5';
+
+module.exports = {
+  get: function(url) {
+    return fetch(rootUrl + url, {
+      headers: {
+        'Authorization': 'Client ID ' + apiKey
+      }
+    }).then(function(response) {
+      return response.json();
+    });
+  }
+};
